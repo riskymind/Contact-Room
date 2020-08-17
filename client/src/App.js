23512996df0b7a449670104f1271af4c6bfc3adc
riskymind <oparaiskelechi@gmail.com>
2020-08-17 15:59:27 +0100

    created the nav component with home and about riute

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home";
+import { About } from "./components/pages/About";
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Contact Home page</div>;
+};
+
+export default Home;
