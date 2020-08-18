4cba3288270ba978df36b7f211c2ee9df9ae1ade
riskymind <oparaiskelechi@gmail.com>
2020-08-18 05:42:10 +0100

    implemented the update and clear functionality

--- client/src/components/contacts/ContactItem.jsx
+++ client/src/components/contacts/ContactItem.jsx
@@ -2,12 +2,19 @@ import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import ContactContext from "../../context/contact/contactContext";
 
-const ContactItem = ({ contact: { id, name, email, phone, type } }) => {
+const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
-  const { deleteContact } = contactContext;
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
+
+  const { id, name, email, phone, type } = contact;
 
   const onDelete = () => {
     deleteContact(id);
+    clearCurrent();
+  };
+
+  const onSetCurrent = () => {
+    setCurrent(contact);
   };
 
   return (
@@ -36,7 +43,9 @@ const ContactItem = ({ contact: { id, name, email, phone, type } }) => {
         )}
       </ul>
       <p>
-        <button className="btn btn-dark btn-sm">Edit</button>
+        <button className="btn btn-dark btn-sm" onClick={onSetCurrent}>
+          Edit
+        </button>
         <button className="btn btn-danger btn-sm" onClick={onDelete}>
           Delete
         </button>
